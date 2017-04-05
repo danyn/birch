2abb21e6ca03622db6b979c1ce0b3e16e403f6b3
Daniel Eisen <eisen.dan@gmail.com>
2017-04-05 16:31:20 -0700

    cleans up the code for birch layered nav and moves it into its own file  birch-layered-nav-js.php currently it adds the assets such as js and soon also css by writing into the head only when they are needed.

--- root/layered-nav/init.php
+++ root/layered-nav/init.php
@@ -1,39 +1,20 @@
 <?php
 
 require 'birch-layered-nav.php';
+require 'birch-layered-nav-js.php';
 	
-
+//initialize the widget
 add_action( 'widgets_init', function(){
 	register_widget( 'Birch_Layered_Nav' );
 });  
 
+//write all necessary assets js, css into the head when widget is active and viewing a product category
+add_action('wp_head','add_birch_layered_nav'); 
 
-function birch_layered_nav_js(){
+
+function add_birch_layered_nav(){
 	if (is_active_widget( false, false, 'birch_layered_nav', true ) && is_product_category() ){
-?>
-<script>
-jQuery( document ).ready( function( $ ) {
-    // $() will work as an alias for jQuery() inside of this function
-    console.log('birch_layered_nav_js');
-	
-//	var query_string = $(location).attr('href').split('?')[1].replace(/filter_/g,'attribute_pa_');
-	var query_string = $(location).attr('href').split('?')[1];
-	if (query_string){
-		query_string = query_string.replace(/filter_/g,'attribute_pa_');
-			var variable_products = $(' li.product-type-variable a');
+			birch_layered_nav_js();
+	}//if active and products shown as archive
+}
 
-		variable_products.each(function(){
-			this.href = this.href + '?' + query_string;
-			console.log(this.href);
-		});
-	}//if there is a query string
-	console.log(query_string);	
-} );//document ready
-</script>	
-<?php  
- }//if widget active
-}//bich_layered_nav_js
-add_action('wp_head','birch_layered_nav_js');
-
-
-//pa_
